e0e8157e3132b0e6bd04a6f5aa884a86ac4a63f8
Melanie West <dhmofan@gmail.com>
2017-09-07 00:22:33 -0400

    Automate character divs for eventual relocation.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 var char=[];
+var charDiv;
 
     char[0] = new charConstructor('Bugs Bunny',"assets/images/bugsicon.jpg",100);
     char[1] = new charConstructor('Road Runner',"assets/images/rricon.png",120);
@@ -8,43 +9,67 @@ var char=[];
     char[3] = new charConstructor('Yosemite Sam',"assets/images/samicon.png",200);
      
 
-// $("#box21").html("ready");
+$(".char").on("click",function(){
 
-// $(".box1").on("click",function(){
-//     $(this).appendTo("#box31");
-// });
+    var charIndex = $(this).val();
+    console.log(charIndex);
+
+    var boxToEmpty = "#box0" + charIndex;
+    $(boxToEmpty).remove();
+
+    charDiv = charCardCreate(charIndex);
+    $("#box20").append(charDiv);   
+});
 
 
 
 
 for (var i=0; i<char.length; i++){
     
-                var charDiv = $('<div class="icons">');  //new div for images
-    
-                var name = $("<p>");
-                var hp   = $("<p>");
-                  
-                var charImg = $('<img>');
+  charCardCreate(i);
 
-                name.html(char[i].name);
-                charImg.attr("src",char[i].url);
-                hp.html(char[i].hp);
+   //for now, locate the characters around the board to see if
+   // the boxes hold their locations in rows and keep the colors
+   //initially they should be put in "#box0"+i (top row).
+   //moving them is proving challenging.
 
-                charDiv.append(name);
-                charDiv.append(charImg);  //put into dynamically created div
-                charDiv.append(hp);
-    
-                var boxNum = "#box3" + i;
-                $(boxNum).append(charDiv);  //add to back
-                }
+  var boxNum = "#box"+ i + i;
+  $(boxNum).append(charDiv);  //add to back
+
+  }
     
 
 
+//object constructor function
 
 function charConstructor(name,url,hp) {
     this.name = name;
     this.url  = url;
     this.hp   = hp;
+}
+
+
+function charCardCreate(index){
+    charDiv = $('<div class="icons">');  //new div for images
+    
+        var name = $("<p>");
+        var hp   = $("<p>");
+            
+        var charImg = $('<img>');
+
+        name.html(char[index].name);
+        charImg.attr("src",char[index].url);
+        charImg.attr("class","char");
+        charImg.attr("width","128px");
+        charImg.attr("value",index);
+        hp.html(char[index].hp);
+
+        charDiv.append(name);
+        charDiv.append(charImg);  //put into dynamically created div
+        charDiv.append(hp);
+
+        return charDiv;
+ 
 }
 
 })
